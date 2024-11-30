5c0b11d9cf23cfe8b393e09c1876ce6001e9de86
edu021 <edu.netgaming@gmail.com>
2024-11-30 17:06:32 -0300

    Atualizando schema do banco

--- cronolove.sql
+++ cronolove.sql
@@ -1,28 +1,4 @@
-/*!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19  Distrib 10.6.18-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: cronolove
--- ------------------------------------------------------
--- Server version	10.6.18-MariaDB-0ubuntu0.22.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `grawe_page`
---
-
 DROP TABLE IF EXISTS `grawe_page`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grawe_page` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `couple_name` varchar(255) NOT NULL,
@@ -33,24 +9,11 @@ CREATE TABLE `grawe_page` (
   `started_at` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `grawe_page`
---
 
 LOCK TABLES `grawe_page` WRITE;
-/*!40000 ALTER TABLE `grawe_page` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grawe_page` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `grawe_user`
---
-
 DROP TABLE IF EXISTS `grawe_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grawe_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -58,24 +21,11 @@ CREATE TABLE `grawe_user` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `grawe_user`
---
 
 LOCK TABLES `grawe_user` WRITE;
-/*!40000 ALTER TABLE `grawe_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grawe_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `temp_images`
---
-
 DROP TABLE IF EXISTS `temp_images`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `temp_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` varchar(255) NOT NULL,
@@ -87,24 +37,6 @@ CREATE TABLE `temp_images` (
   `plan` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `temp_images`
---
 
 LOCK TABLES `temp_images` WRITE;
-/*!40000 ALTER TABLE `temp_images` DISABLE KEYS */;
-/*!40000 ALTER TABLE `temp_images` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-11-30 13:06:17
