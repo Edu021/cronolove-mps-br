d4460c4c4aaf654c187e7b04d964f0710f9b8af8
Edu021 <edu.netgaming@gmail.com>
2024-12-02 13:29:28 -0300

    Organizando repositório

--- main.js
+++ main.js
@@ -29,8 +29,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/teste', (req, res) => {
+    console.log(__dirname);
     res.sendFile(path.join(__dirname, 'views/html/', 'teste.html'));
 });
+
 app.get('/form.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/css/', 'form.css'));
 });
@@ -42,6 +44,7 @@ app.get('/template.css', (req, res) => {
 app.get('/footer.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/css/', 'footer.css'));
 });
+
 app.get('/teste.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/css/', 'teste.css'));
 });
